3d9b40cc787164e4668bfaf56d0574dd2304f3d3
heardofreyy <abderrahmenebendoukha8@gmail.com>
2023-03-07 07:35:47 -0600

    aziz

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBus } from "@fortawesome/free-solid-svg-icons";
+import { faBusSimple } from "@fortawesome/free-solid-svg-icons";
 
 
 
 export default function Header() {
     return (
         <header>
-            <div className="logo"><FontAwesomeIcon icon ={faBus}></FontAwesomeIcon></div>
+            <div className="logo"><FontAwesomeIcon icon ={faBusSimple}></FontAwesomeIcon></div>
             <div className="header-btns">
                 <Link to={"/"}><button className="h-btn" >Home</button></Link>
                 <Link to={"/page/1"}><button className="h-btn">page1</button></Link>
