4f61d88da1d091f46cccb216187ac580b59f96d6
= <=>
2023-03-08 14:03:15 +0100

    :construction: setup global state

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -5,8 +5,15 @@ import { faBusSimple } from "@fortawesome/free-solid-svg-icons";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 import { motion } from "framer-motion";
+import { ApplicationContext } from "../../contexts/ApplicationContext";
 
 export default function Header() {
+    const context = React.useContext(ApplicationContext);
+
+    React.useEffect(() => {
+        console.log(context);
+    }, []);
+
     return (
         <motion.header initial={{ opacity: 0, y: -128 }} animate={{ opacity: 1, y: 0 }}>
             <div className="header-container">
