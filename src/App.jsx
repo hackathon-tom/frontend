703aaaeb289bc4735faa0c1fe6d72cc7f6081507
heardofreyy <abderrahmenebendoukha8@gmail.com>
2023-03-08 05:44:29 -0600

    pushit

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ import Footer from './assets/components/Footer';
 import Home from './assets/pages/Home';
 import Bus from './assets/pages/Bus';
 import Information from './assets/pages/Information';
-import Choose from './assets/components/Choose';
 import MapPage from './assets/pages/MapPage';
 import Login from './assets/pages/Login';
 import Signup from "./assets/pages/Signup";
@@ -20,7 +19,6 @@ import './assets/CSS/App.css'
 
 
 export default function App() {
-<<<<<<< HEAD
     const pathNow = location.pathname;
 
     if (pathNow == '/map') {
@@ -34,18 +32,11 @@ export default function App() {
         
             <>
                 <Header />
-=======
-    return (
-        <div className='main-container'>
-            <Header />
-            <div className="page-container">
->>>>>>> f51f487d9e5393cbdce3d46975d5baf3706afba7
                 <Routes >
                     <Route path='/' element={<Home />} />
                     <Route path='/bus' element={<Bus />} />
                     <Route path='/information' element={<Information />} />
                     <Route path='/login' element={<Login />} />
-<<<<<<< HEAD
                     
                     <Route path='/api' element={<ApiTesting />} />
                 </Routes>
@@ -54,17 +45,6 @@ export default function App() {
         );
     }
     
-=======
-                    <Route path='/signup' element={<Signup />} />
-                    <Route path='/choose' element={<Choose />} />
-                    <Route path='/choose/map' element={<Map />} />
-                    <Route path='/api' element={<ApiTesting />} />
-                </Routes>
-            </div>
-            {/*<Footer />*/}
-        </div>
-    );
->>>>>>> f51f487d9e5393cbdce3d46975d5baf3706afba7
 }
 
 
