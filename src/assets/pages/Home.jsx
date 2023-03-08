db36cc2e10c54d230d4ee2eab912663d7cfb5ec5
= <=>
2023-03-08 12:54:17 +0100

    pause

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -6,6 +6,8 @@ import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
 import Dest from "../components/Dest";
 import CallToAction from "../components/CallToAction";
 
+import "../CSS/Pages.css";
+
 export default function Home() {
     return (
         <div className='home'>
@@ -15,11 +17,7 @@ export default function Home() {
                     <p className="hero-descriptipn">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro expedita fuga fugiat quas iure eos quidem perferendis inventore </p>
                 </div>
                 <Dest />
-<<<<<<< HEAD
-                <Link to="/map"><button className="hero-btn"><FontAwesomeIcon icon={faMapMarkerAlt} /> &nbsp; find your way</button></Link>
-=======
                 <CallToAction />
->>>>>>> f51f487d9e5393cbdce3d46975d5baf3706afba7
             </div>
             <div className="hero-right">
                 <div className="hero-image" />
