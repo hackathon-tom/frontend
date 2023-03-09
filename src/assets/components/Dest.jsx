fa5a0b59d88483a411a4e3e72a0de856572681cc
heardofreyy <abderrahmenebendoukha8@gmail.com>
2023-03-09 04:20:33 -0600

    commit

--- src/assets/components/Dest.jsx
+++ src/assets/components/Dest.jsx
@@ -1,16 +1,55 @@
 import React from 'react'
 
-import { faMapMarkerAlt, faEllipsisVertical, faCircleDot } from '@fortawesome/free-solid-svg-icons'
+import { faMapMarkerAlt, faEllipsisVertical, faCircleDot, faLocationCrosshairs, faMapPin } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 import '../CSS/Dest.css'
 
+
+
+
 export default function Dest() {
+  
+  const [inputWinrah, setInputWinRah] = React.useState();
+
+  React.useEffect(()=> {setInputWinRah(document.getElementById('winrah'))}, [])
+
+  
+  let winRah = {lat:'', lng:''}
+
+  function setValueInputDest () {
+
+    let val = winRah.lat + ' / ' + winRah.lng;
+    return val;
+  }
+
+  function getLocation (e) {
+    const successCallback = (position) => {
+      winRah.lat = position.coords.latitude;
+      winRah.lng = position.coords.longitude;
+      let jsp = setValueInputDest();
+      console.log(jsp)
+      console.log(inputWinrah)
+      inputWinrah.value = jsp;
+    };
+    
+    const errorCallback = (error) => {
+      console.log(error);
+    };
+    
+    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+
+  }
+
+  
+
   return (
     <div className='Dest'>
         <div className="dest-destination">
             <FontAwesomeIcon icon={faCircleDot}></FontAwesomeIcon>
-            <input type="text" placeholder='choose your location'/>
+            <input id='winrah' type="text" placeholder='choose your location' />
+            <button className='gps' onClick={getLocation}> <FontAwesomeIcon icon={faLocationCrosshairs}></FontAwesomeIcon> </button>
         </div>
         <div className="separi">
             <FontAwesomeIcon icon={faEllipsisVertical}></FontAwesomeIcon>
@@ -18,6 +57,7 @@ export default function Dest() {
         <div className="dest-arrive">
             <FontAwesomeIcon icon={faMapMarkerAlt}></FontAwesomeIcon>
             <input type="text" placeholder='choose your destination'/>
+            <button className='gps' > <FontAwesomeIcon icon={faMapPin}></FontAwesomeIcon> </button>
         </div>
     </div>
   )
--- src/assets/components/Map.jsx
+++ src/assets/components/Map.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
-import { useMemo } from 'react';
+import React, { useRef, useState } from 'react';
 
-import { GoogleMap,useJsApiLoader, Marker, DirectionsService, DirectionsRenderer,  } from '@react-google-maps/api';
+import { GoogleMap,useJsApiLoader, Marker, DirectionsService, DirectionsRenderer,  Autocomplete} from '@react-google-maps/api';
 
 const containerStyle = {
   width: '100%',
   height: '100vh'
 };
 
-const center = {
+const buttonStyle = {
+  position: 'absolute',
+  right: '0',
+  zIndex: '999'
+}
+
+const ese = {
   lat: 35.70310713370488,
   lng: -0.5833646831049463
+};
+const center = {
+  lat: 35.723533,
+  lng: -0.569790
 };
 
 
@@ -20,16 +29,44 @@ export default function Map() {
   
   const { isLoaded } = useJsApiLoader({ id: 'google-map-script', googleMapsApiKey: ""})
   
+  const [directionsResponse, setDirectionsResponse] = useState(null);
+  const [distence, setDistence] = useState('');
+  const [duration, setDuration] = useState('');
 
+  const originRef = useRef(ese);
+  const destinationRef = useRef(center);
+
+  async function calculateRoute() {
+    if(originRef.current.value === '' || destinationRef.current.value === ''){
+      return
+    }
+    const directionsService = new google.maps.DirectionsService();
+    const result = await directionsService.route({
+      origin: ese,
+      destination: center,
+      travelMode: google.maps.TravelMode.DRIVING
+    })
+
+    
+
+    setDirectionsResponse(result);
+    setDistence(result.routes[0].legs[0].distance.text);
+    setDuration(result.routes[0].legs[0].duration.value);
+  }
 
   return (
     <div className='map-container'>
+      <button style={buttonStyle} className='oooooo' onClick={calculateRoute}>oooooo</button>
       <GoogleMap
         mapContainerStyle={containerStyle}
         center={center}
         zoom={13}
+        
+        
       >
         <Marker position={center}/>
+        <Marker position={ese}/>
+        {directionsResponse && <DirectionsRenderer directions={directionsResponse}/>}
       </GoogleMap>
     </div>
   
